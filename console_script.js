9dc4f8792033aeb5591d9d1b8c8149e2fad6f63b
Josh Braun <josh.braun@adurolife.com>
2018-02-09 13:36:15 -0800

    Fix errors and grab weird edge cases of the CTAs

--- console_script.js
+++ console_script.js
@@ -20,27 +20,42 @@ function updateHtml() {
   const coachingProgramsContainer = document.querySelector('.coaching-programs-container');
 
   //// BEGIN update all four tiles ////
-  const appCta = 'https://mywellnessnumbers.com/HumanPerformance/images/2018_banners/Ember_CTA.png'
+  const appCta = 'https://mywellnessnumbers.com/HumanPerformance/images/2018_banners/Ember_CTA.png';
+  const appCta2 = 'https://mywellnessnumbers.com/HumanPerformance/images/2018_banners/aduro_app_CTA.png';
   const oneOneCta = 'https://mywellnessnumbers.com/HumanPerformance/images/2018_banners/Coaching_CTA.png';
+
+  // Remove this junk after 2/9 update
+  const pineapple = `alt="AMP callout"`;
+  const pineapple2 = `alt="AMP callout" style="width: 100%;"`;
+  const pineapple3 = `alt="Ember callout"`;
+
+  if(!originalHtml.includes(pineapple2)) {
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(pineapple , pineapple2);
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(pineapple3 , pineapple2);
+  }
 
   // are we in Health & Fitness?
   if (originalHtml.includes('Fitness focuses')) {
     tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_HF.png');
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta2, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_HF.png');
     tileDescription.innerHTML = tileDescription.innerHTML.replace(oneOneCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/Coaching_CTA_HF.png')
   }
   // are we in Money & Prosperity?
   if (originalHtml.includes('Prosperity focuses')) {
     tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_MP.png');
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta2, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_MP.png');
     tileDescription.innerHTML = tileDescription.innerHTML.replace(oneOneCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/Coaching_CTA_MP.png')
   }
   // are we in Growth & Development?
   if (originalHtml.includes('Development focuses')) {
     tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_GD.png');
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta2, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_GD.png');
     tileDescription.innerHTML = tileDescription.innerHTML.replace(oneOneCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/Coaching_CTA_GD.png')
   }
   // are we in Contribution & Sustainability?
   if (originalHtml.includes('Sustainability focuses')) {
     tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_CS.png');
+    tileDescription.innerHTML = tileDescription.innerHTML.replace(appCta2, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/App_CTA_CS.png');
     tileDescription.innerHTML = tileDescription.innerHTML.replace(oneOneCta, 'https://mywellnessnumbers.com/HumanPerformance/images/banners/Coaching_CTA_CS.png')
   }
 
